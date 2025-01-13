7ccf50902cc6927732f9b0dd3bbb528211eeb0cd
Vegante24 <lucasvega2006@gmail.com>
2025-01-13 12:39:49 -0300

    #00 - Python

---
+++ Vegante24.py
@@ -0,0 +1,40 @@
+#link de pagina oficial de pyton: https://www.python.org/
+
+
+#comentario en una linea
+"hola"
+
+
+#en varias lineas
+"""
+hola
+buenas
+tardes
+"""
+
+#variables
+ejemplo_variable = "empanada de queso"
+ejemplo_variable = "empanada de pino" #esta variable aunque sea la misma, toma el valor de la ultima agregada y omite la anterior.
+
+#constante
+EJEMPLO_CONSTANTE = "pizza" #que este en mayuscula, significa que la variable no debe cambiar
+#aunque python no tenga constantes, esta hace referencia a una para que la gente sepa que es una constante y no la cambie
+
+#tipos de datos primitivos
+
+#string (cadena)
+ejemplo_string = "invadan peru"
+
+#int (entero)
+ejemplo_int = 100
+
+#float (numeros decimales)
+ejemplo_decimales = 0.5
+
+#bool (booleano, puede ser true y false)
+ejemplo_bool1 = True
+ejemplo_bool2 = False
+
+print("¡Hola, python!")
+print("ARRIBA ESPAÑA")
+print("y viva chile")
